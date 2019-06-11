68671b35f114d368a58d01a65d425fe16b3a1358
svovanusus <38732208+svovanusus@users.noreply.github.com>
2019-06-11 14:19:59 +0300

    Update EventModel.java

--- src/main/java/com/ccalendar/server/db/model/EventModel.java
+++ src/main/java/com/ccalendar/server/db/model/EventModel.java
@@ -54,7 +54,7 @@ public class EventModel implements Serializable {
             inverseJoinColumns = { @JoinColumn(name = "genre_id") })
     private Set<GenreModel> genresForEvent = new HashSet<>();
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     @JoinTable(name = "user_event",
             joinColumns = { @JoinColumn(name = "event_id") },
             inverseJoinColumns = { @JoinColumn(name = "user_id") })
